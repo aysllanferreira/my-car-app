97408bc0d1aa12978706ab69940b8dff6dd6a077
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-02-25 17:20:50 -0300

    feat: Create user in db is now working

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
 export class ReportsService {}
